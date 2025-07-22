cada82394fe8cd80723513dd860a19bb0b23d4cf
gunjalk <kishor.gunjal@autodesk.com>
2025-07-22 12:02:47 +0530

    Initial commit

---
+++ src/main/java/com/spring/SpringBootApp/SpringBootAp.java
@@ -0,0 +1,15 @@
+package com.spring.SpringBootApp;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAp.class, args);
+		System.out.println("Spring Boot Application Started");
+	}
+
+}
---
+++ src/test/java/com/spring/SpringBootApp/SpringbootmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.SpringBootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
